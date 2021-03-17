86391f45704077c834cdfafc1d26a32172ef225c
ej <ej@emjames.com>
2021-03-17 19:20:09 +0800

    [xrandr] Add ccw3screen

---
+++ .screenlayout/ccw3screen.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1080x1369 --rotate normal --output DP1 --mode 1920x1080 --pos 0x0 --rotate left --output DP2 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 1080x289 --rotate normal --output VIRTUAL1 --off
+i3-msg 'workspace "3:3"; move workspace to output DP1'
+i3-msg 'workspace "2:2"; move workspace to output HDMI2'
+
